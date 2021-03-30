331f8ad3f844d1aa470d23eff259981fa0cc1173
SNmcdarby1 <sngereso@gmail.com>
2021-03-30 21:02:49 +0000

    edit the table, and images responsive size

--- run.py
+++ run.py
@@ -12,26 +12,27 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/Activo")
 def activo():
-    return render_template("activo.html")
+    return render_template("activo.html", page_title="Activo")
 
 
 @app.route("/Recipes")
 def recipe():
-    return render_template("recipe.html")
+    return render_template("recipe.html", page_title="Recipe", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", page_title="Login")
 
 
 if __name__ == "__main__":
