b77e602c1389864de2ae2d6f63a0144343291c7d
SNmcdarby1 <sngereso@gmail.com>
2021-03-31 19:13:43 +0000

    git add membership details

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -19,6 +22,17 @@ def about():
     return render_template("about.html", page_title="About", activo=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/activo.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/Activo")
 def activo():
     return render_template("activo.html", page_title="Activo")
@@ -29,14 +43,17 @@ def recipe():
     return render_template("recipe.html", page_title="Recipe",)
 
 
-@ app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
-@ app.route("/login")
-def login():
-    return render_template("login.html", page_title="Login")
+@ app.route("/member")
+def member():
+    return render_template("member.html", page_title="member")
 
 
 if __name__ == "__main__":
